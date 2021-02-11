b21e505e08caa1f58a95d9b9af657081a8983498
Wrazlmumfp <bernhard.andraschko@gmx.de>
2021-02-11 14:30:06 +0100

    moved main_default.tex to main.tex

--- compile.sh
+++ compile.sh
@@ -1,6 +1,9 @@
 #pdflatex -output-directory="temp" $1
-filename=$(basename -- "$1")
-filename="${filename%.*}"
-pdflatex -output-directory="temp" $1
-pdflatex -output-directory="temp" $1
-mv temp/$filename.pdf .
+#filename=$(basename -- "$1")
+#filename="${filename%.*}"
+#pdflatex -output-directory="temp" $1
+#pdflatex -output-directory="temp" $1
+#mv temp/$filename.pdf .
+pdflatex -output-directory="temp" main.tex
+pdflatex -output-directory="temp" main.tex
+mv temp/main.pdf .
